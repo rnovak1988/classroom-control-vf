98efd72a064f5766ea23c0095b98a5935695c8fb
root <root@rnovak1988.puppetlabs.vm>
2016-06-28 18:25:10 +0000

    removed notify

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,7 +42,6 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  notify { "Hello, my name is ${::hostname}": }
 
   user { 'robertnovak':
     name   => 'robertnovak',
