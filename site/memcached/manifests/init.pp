4020b2068d1f884166edc67d4b3c4459a2d2fb98
Robert Novak <me@robert-novak.com>
2016-06-29 16:27:52 +0000

    add file management for the sysconfig configuration for memcached

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -2,4 +2,11 @@ class memcached {
   package { 'memcached':
     ensure => present
   }
+  file { 'memcached_config':
+    path    => '/etc/sysconfig/memcached',
+    source  => 'puppet:///modules/memcached/memcached_conf',
+    owner   => 'root',
+    mode    => '0644',
+    require =>  Package['memcached']
+  }
 }
